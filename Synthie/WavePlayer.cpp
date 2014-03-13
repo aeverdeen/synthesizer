dd97ce3efa2864250156d3fd5af12f3ff4d21b20
mcquadeh <hmm9254@yahoo.com>
2014-03-13 19:29:21 -0400

    Update WavePlayer.cpp

--- Synthie/WavePlayer.cpp
+++ Synthie/WavePlayer.cpp
@@ -32,7 +32,7 @@ bool CWavePlayer::Generate()
 	//	m_frame[0] = wave+wave2; //trying to do glissando
 		m_frame[0] = (m_samples[m_position] / 32768.0);
         m_frame[1] = m_frame[0];
-		m_position++;
+		m_position=m_position+2;
 
 
 		//The following is trying to implement the Envelope Generation
